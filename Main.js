b9a75fa6ce3e7ef8ea208d58e2cef7aec6f6ce61
Tinoisthe <76804686+Tinoisthe@users.noreply.github.com>
2021-03-17 19:14:44 +1100

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,64 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ const prefix = '!';
+
+const fs = require('fs');
+
+const command = ('command');
+
+const commands = ('commands');
+
+async  =>
+  client.login(process.env.BOT_TOKEN);
+  client.commands = new Map();
+  client.cachedmessagereactions = new Map();
+  
+
+
+
+
+client.commands = new Discord.Collection();
+
+const commandfiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandfiles){
+const command = require(`./commands/${file}`);
+
+  client.commands.set(command.name, command);
+}
+
+
+
+
+
+ 
+
+
+
+client.once('ready',() => {
+    console.log('Tino is online!');
+});
+
+client.on('message', message => {
+ if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+ const args = message.content.slice(prefix.length).split(/ +/);
+ const commands = args.shift().toLowerCase();
+ 
+
+ if(command === '!ping'){
+
+  client.commands.get('ping').execute(message, args);
+    
+    
+
+
+
+    
+    }
+  
+   });
+
+
+client.login('NzkzNzM0OTg3NTEzODU2MDIw.X-wlTw.cg2EBnGmfLFRKHDLKslyoXdRAA0')
