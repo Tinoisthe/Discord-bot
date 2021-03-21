1b4c6686bf63a9a8b8e1772600fd26637b308bd5
unknown <Tysonsunderland15@gmail.com>
2021-03-21 21:36:25 +1100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,24 +1,22 @@
 module.exports = {
-    name: 'ban',
-    description: "this command bans a member!",
-    execute(message, args){
-           const member = message.mentions.users.first();
-             if(member){
-              if(message.member.roles.cache.has('<@701021515524866118>')){
+  name: 'ban',
+  description: "this command ban a member!",
+  execute(message, args){
+         const member = message.mentions.users.first();
+           if(member){
+            if(message.member.roles.cache.has('<@701021515524866118>')){
                 
-                const memberTarger = message.guild.members.cache.get(member.id)
-                memberTarger.ban();
-                message.channel.send("User has been ban")
-             }else{
-                   message.channel.send('You cant ban that member');
-   
-                  
-                      
-                      
-                            
-                   }
-               }
-   
-    }
-   
+              const memberTarger = message.guild.members.cache.get(member.id)
+              memberTarger.ban();
+              message.channel.send("User has been ban");
+           }else{
+                 message.channel.send('You cant ban that member');
+
+                                  
+                }
+              }
+  
    }
+  
+}
+  
