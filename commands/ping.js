ae9747fea1b043ed3837357ec5ee11d6b457a8f9
unknown <Tysonsunderland15@gmail.com>
2021-03-31 20:24:09 +1100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,12 +5,13 @@ module.exports = {
 
 
       if(message.member.roles.cache.has('<@412011486953865227>')){
+           
        message.channel.send('pong!')
        } else {
              message.channel.send("you can't send this Because you don't have the Authorisation ");
-            
+             
        }
   
  }
  
- }
+}
