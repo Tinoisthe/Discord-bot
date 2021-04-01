5eeea742b554b61fd2ee474e4de5c6351b2f5dcc
unknown <Tysonsunderland15@gmail.com>
2021-04-01 22:53:25 +1100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,11 +4,11 @@ name: 'help',
   description: "this command help a member!",
   execute(message, args){
     
-              message.channel.send("!ban you need Admin");
-              message.channel.send("!kick you need Admin");
-              message.channel.send("!clear [add number]");
-              message.channel.send("!ping");
-              message.channel.send("!tino");
+         ///     message.channel.send("!ban you need Admin");
+            //  message.channel.send("!kick you need Admin");
+              message.channel.send(">clear [add number]");
+              message.channel.send(">ping");
+              message.channel.send(">tino");
          
  
                      
