765143de4cda6e9e47329ec9c59183a7ebc475c4
Tinoisthe <Tysonsunderland15@gmail.com>
2021-06-29 20:36:15 +1000

    Update admin.js

--- commands/admin.js
+++ commands/admin.js
@@ -2,37 +2,11 @@ module.exports = {
     name: 'admin',
     description: "this command get admin commands",
     execute(message, args){
-
-        if(message.member.roles.cache.has('786517237703639080')){
-            message.channel.send('You cant use that so i will send this');
-            message.channel.send("HI");
-        }else{
-      
-        if(message.member.roles.cache.has('808044039282622544')){
-            message.channel.send('You cant use that so i will send this');
-            message.channel.send("HI");
-        }else{
-            
-            if(message.member.roles.cache.has('737989008218652733')){
+            if(message.member.roles.cache.has('688214236765356044')){
                 message.channel.send(">kick command");
                 message.channel.send(">ban command");
             }else{
                 message.channel.send('You cant use that');
-          
-                if(message.member.roles.cache.has('786517329981866015')){
-                message.channel.send(">kick command");
-                message.channel.send(">ban command");
-             }else{
-                   message.channel.send('You cant use that');
-   
-                       
-                      
-                            
-                   }
-               }
-   
-    }
-
-}
-}
+            }
+        }
 }
