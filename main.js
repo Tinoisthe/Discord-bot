82866863f3f43db9786bbf0acc4a791a1936737e
Tinoisthe <Tysonsunderland15@gmail.com>
2021-07-14 16:17:17 +1000

    Update main.js

--- main.js
+++ main.js
@@ -119,7 +119,7 @@ console.log(cmd);
 
 client.once('ready',() => {
     console.log('Tino is online!')
-    client.user.setActivity('over 100 servers', { type: 'WATCHING'});
+    client.user.setActivity('over kaalinks.xyz', { type: 'WATCHING'});
 });
 
 client.on('message', message => {
