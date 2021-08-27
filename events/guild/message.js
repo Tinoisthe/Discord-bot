c336636a975d093760272b8ccb8d21b7f259c77e
Tinoisthe <Tysonsunderland15@gmail.com>
2021-08-27 23:54:32 +1000

    work

--- events/guild/message.js
+++ events/guild/message.js
@@ -4,6 +4,9 @@ module.exports = (Discord, client, message) => {
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const cmd = args.shift().toLowerCase();
-    const command = client.commands.get(cmd);
+    if(cmd.length===0) return; //return if it's only prefix without command
+    let command = client.commands.get(cmd) //get command from command folder
+    || client.commands.get(client.aliases.get(cmd)); //get command if command's alias is used
+   
     if(command) command.execute(client, message, args, cmd, Discord);
 }
